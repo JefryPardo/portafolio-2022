cbfd8e58747dd642ed1c3e575e049f2067c21d62
jefry johan pardo alzate <jefry.pardo@tucompra.com.co>
2022-01-27 15:46:14 -0500

    subir a github pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portafolio-2022';
+  
+  items: MenuItem[];
+
+  activeItem: MenuItem;
+  
+  constructor() {
+    this.items = [
+      {label: 'Home', icon: 'pi pi-fw pi-home', command: (event) => {this.holaMundo(event)}},
+      {label: 'Calendar', icon: 'pi pi-fw pi-calendar', command: (event) => {this.holaMundo2(event)}},
+      {label: 'Edit', icon: 'pi pi-fw pi-pencil'},
+      {label: 'Documentation', icon: 'pi pi-fw pi-file'},
+      {label: 'Settings', icon: 'pi pi-fw pi-cog'}
+    ];
+
+    this.activeItem = this.items[3];
+  }
+
+  holaMundo(name: any) {
+    console.log(name);
+  }
+
+  holaMundo2(name: any) {
+    console.log(name);
+  }
 }
